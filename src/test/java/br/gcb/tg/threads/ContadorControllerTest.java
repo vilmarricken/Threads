18063f60a77d14ca2aa5b810c09161dfec7de1ce
VILMAR RICKEN <vilmar.ricken@viavarejo.com.br>
2024-08-05 18:20:34 -0300

    feat: Incrementando testes

--- src/test/java/br/gcb/tg/threads/ContadorControllerTest.java
+++ src/test/java/br/gcb/tg/threads/ContadorControllerTest.java
@@ -6,10 +6,24 @@ import org.junit.Test;
 public class ContadorControllerTest {
 	
 	@Test
-	public void contar100() {
+	public void contar1000() {
 		Contador contador = new Contador();
 		ContadorController contadorController = new ContadorController(contador);
-		Assert.assertEquals(100, contadorController.contar(10));
+		Assert.assertEquals(1000, contadorController.contar(100));
+	}
+
+	@Test
+	public void contar10000() {
+		Contador contador = new Contador();
+		ContadorController contadorController = new ContadorController(contador);
+		Assert.assertEquals(10000, contadorController.contar(1000));
+	}
+
+	@Test
+	public void contar100000() {
+		Contador contador = new Contador();
+		ContadorController contadorController = new ContadorController(contador);
+		Assert.assertEquals(100000, contadorController.contar(10000));
 	}
 
 }
