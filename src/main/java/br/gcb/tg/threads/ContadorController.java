7ee3a15a7b234189ce730641f0251d4a1ab14426
VILMAR RICKEN <vilmar.ricken@viavarejo.com.br>
2024-08-05 21:31:52 -0300

    feat: ajustes

--- src/main/java/br/gcb/tg/threads/ContadorController.java
+++ src/main/java/br/gcb/tg/threads/ContadorController.java
@@ -5,34 +5,24 @@ import java.util.List;
 
 public class ContadorController {
 
-	private Contador contador;
-
-	private int iteracoes;
-	
-	Runnable run = new Runnable() {
-		public void run() {
-			for (int i = 0; i < iteracoes; i++) {
-				contador.incrementar();
-			}
-		}
-	};
-	
-	public ContadorController(Contador contador) {
-		this.contador = contador;
+	public ContadorController() {
 	}
 
-	public Object contar(int iteracoes) {
-		this.iteracoes = iteracoes;
+	public Object contar(final int iteracoes) {
+		final Contador contador = new Contador();
+		Runnable run = new Runnable() {
+			public void run() {
+				for (int i = 0; i < iteracoes; i++) {
+					contador.incrementar();
+				}
+			}
+		};
+
 		List<Thread> threads = new ArrayList<Thread>();
 		for (int i = 0; i < 10; i++) {
 			Thread thread = new Thread(run);
 			threads.add(thread);
 			thread.start();
-		}
-		try {
-			Thread.sleep(1);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
 		}
 		return contador.contador();
 	}
--- src/test/java/br/gcb/tg/threads/ContadorControllerTest.java
+++ src/test/java/br/gcb/tg/threads/ContadorControllerTest.java
@@ -7,29 +7,25 @@ public class ContadorControllerTest {
 	
 	@Test
 	public void contar100() {
-		Contador contador = new Contador();
-		ContadorController contadorController = new ContadorController(contador);
+		ContadorController contadorController = new ContadorController();
 		Assert.assertEquals(100, contadorController.contar(10));
 	}
 
 	@Test
 	public void contar1000() {
-		Contador contador = new Contador();
-		ContadorController contadorController = new ContadorController(contador);
+		ContadorController contadorController = new ContadorController();
 		Assert.assertEquals(1000, contadorController.contar(100));
 	}
 
 	@Test
 	public void contar10000() {
-		Contador contador = new Contador();
-		ContadorController contadorController = new ContadorController(contador);
+		ContadorController contadorController = new ContadorController();
 		Assert.assertEquals(10000, contadorController.contar(1000));
 	}
 
 	@Test
 	public void contar100000() {
-		Contador contador = new Contador();
-		ContadorController contadorController = new ContadorController(contador);
+		ContadorController contadorController = new ContadorController();
 		Assert.assertEquals(100000, contadorController.contar(10000));
 	}
 
